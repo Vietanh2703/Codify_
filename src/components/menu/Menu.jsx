e10d3beccb963866b8c52c481e7340b84a7c4a10
Nguyen Trung Viet Anh <vietanh2732004@gmail.com>
2024-11-14 19:11:56 +0700

    Update navbar, menu and footer

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,13 +1,19 @@
-import React from 'react';
 import './Menu.css';
+import home from "../../assets/home.png";
+import {Link} from "react-router-dom";
 
 function Menu() {
     return (
-        <footer className="menu">
-            <div>
-                <h2>Menu</h2>
+        <div className="menu">
+            <div className="items">
+                <Link to={"/"} className="listItems">
+                    <img src={home} alt="" />
+                    <span className="listItemTitle">Home</span>
+                </Link>
+                <span className="title">My Courses</span>
+                <Link to={"/courses"}>Courses</Link>
             </div>
-        </footer>
+        </div>
     );
 }
 
