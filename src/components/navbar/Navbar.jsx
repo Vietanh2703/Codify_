d81e69458ce3af95e71ce0270f9a166e745553bb
Nguyen Trung Viet Anh <vietanh2732004@gmail.com>
2024-11-14 13:13:31 +0700

    Update navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,26 +1,38 @@
 import './Navbar.css';
 import image from '../../assets/logo.svg';
 import searchIcon from '../../assets/search.svg';
+import notificationIcon from '../../assets/notification.svg';
+import profileIcon from '../../assets/demo-avatar.svg';
+import {useState} from "react";
 function Navbar() {
+    const [searchText, setSearchText] = useState('');
     return (
         <nav className="navbar">
             <div className="logo">
                 <img src={image} alt=""/>
-                <span style={{fontWeight: 'bold', fontSize: 40}}>Codify_</span>
-                <div className="search-container">
-                    <img src={searchIcon} alt="Search" className="search-icon" onClick={() => {
-                        document.querySelector('.search-bar').classList.toggle('active');
-                    }}/>
-                    <div className="search-bar">
-                        <input type="text" placeholder="Search"/>
-                        <button type="submit">
-                            <img src={searchIcon} alt="Search"/>
-                        </button>
-                    </div>
-                </div>
+                <span style={{fontWeight: 'bold', fontSize: 30, marginTop: -40}}>Codify_</span>
+            </div>
+            <div className="search-bar">
+                <img
+                    src={searchIcon}
+                    alt="Search"
+                    className={searchText ? '' : 'disabled'}
+                />
+                <input
+                    type="text"
+                    placeholder="Search for anything"
+                    value={searchText}
+                    onChange={(e) => setSearchText(e.target.value)}
+                />
+            </div>
+            <div className="notification">
+                <img src={notificationIcon} alt="Notification"/>
+            </div>
+            <div className={"profile"}>
+                <img src={profileIcon} alt="Profile"/>
             </div>
         </nav>
-);
+    );
 }
 
 export default Navbar;
